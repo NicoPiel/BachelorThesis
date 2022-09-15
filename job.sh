3325b59ebcde71b6765655cd09270d1c21a92ea5
Nico Piel <nico.piel@hotmail.de>
2022-09-15 10:08:26 +0200

    Some more preprocessing changes

--- job.sh
+++ job.sh
@@ -2,7 +2,7 @@
 #SBATCH --partition=gpu4          # partition (queue)
 #SBATCH --nodes=1                # number of nodes
 #SBATCH --mem=64G                # memory per node in MB (different units with suffix K|M|G|T)
-#SBATCH --time=2:00:00           # total runtime of job allocation (format D-HH:MM:SS; first parts optional)
+#SBATCH --time=8:00:00           # total runtime of job allocation (format D-HH:MM:SS; first parts optional)
 #SBATCH --output=slurm.%j.out    # filename for STDOUT (%N: nodename, %j: job-ID)
 #SBATCH --error=slurm.%j.err     # filename for STDERR
 
@@ -10,5 +10,5 @@ module load cuda/10.2
 mamba init
 mamba activate mm
 
-jupyter nbconvert --to script training.ipynb
-srun python3 training.py
+jupyter nbconvert --to script training_nn.ipynb
+srun python3 training_nn.py
---
+++ pre.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --partition=hpc         # partition (queue)
+#SBATCH --nodes=1                # number of nodes
+#SBATCH --mem=8G                # memory per node in MB (different units with suffix K|M|G|T)
+#SBATCH --time=24:00:00           # total runtime of job allocation (format D-HH:MM:SS; first parts optional)
+#SBATCH --output=slurm.%j.out    # filename for STDOUT (%N: nodename, %j: job-ID)
+#SBATCH --error=slurm.%j.err     # filename for STDERR
+
+mamba init
+mamba activate mm
+
+jupyter nbconvert --to script preprocessing_rd.ipynb
+srun python3 preprocessing_rd.py
